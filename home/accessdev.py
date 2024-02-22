d3dcd8100409cff84f4de169d24648e001529cb3
Trin Thana <trinthana@gmail.com>
2024-02-22 08:37:15 +0700

    Fix access

--- home/accessdev.py
+++ home/accessdev.py
@@ -47,7 +47,7 @@ class AccessDev:
                     "first_name": user.first_name,
                     "last_name": user.last_name,
                     "email_address": user.email,
-                    "member_customer_identifier": user.username.upper(),  
+                    "member_customer_identifier": "-VSID" + str(user.id).upper(),  
                     "member_status": "OPEN",  
                 }
                 members.append(member_data)
